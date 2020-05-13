c941bb7cd48ee34cd4473121a96d00361be8fd47
gulliver159 <62343018+gulliver159@users.noreply.github.com>
2020-05-13 20:43:09 +0600

    Added moving cells horizontally, with accounting logic of the game

--- Logics.py
+++ Logics.py
@@ -1,11 +1,13 @@
 import random as rnd
 
+
 # Вывод массива
 def pretty_print(mas):
     print("-" * 10)
     for row in mas:
         print(*row)
-      
+
+
 # Возвращает порядковые номера всех пустых клеток поля
 def get_empty_list(mas):
     lenght_mas = len(mas[0])
@@ -16,21 +18,68 @@ def get_empty_list(mas):
                 Empty.append(get_number_from_index(mas, i, j))
     return Empty
 
+
 # Проверяет, есть ли на поле пустые ячейки
 def is_zero_in_mas(mas):
     for row in mas:
         if 0 in row:
             return True
     return False
-    
-                
+
+
 # Возвращает порядковый номер ячейки поля
 def get_number_from_index(mas, i, j):
     return i * len(mas[0]) + j + 1
+
 
 # Возвращает номер строки и столбца ячейки поля по ее порядковому номеру
 def get_index_from_number(mas, num):
     lenght_mas = len(mas[0])
     num -= 1
-    x, y = num//lenght_mas, num % lenght_mas
+    x, y = num // lenght_mas, num % lenght_mas
     return x, y
+
+
+## Реализация перемещения ячеек по логике игры ##
+# При свайпе влево
+def move_left(mas):
+    lenght_mas = len(mas[0])
+    for row in mas:
+        while 0 in row:
+            row.remove(0)  # Удаляем все нули на поле
+        while len(row) != lenght_mas:
+            row.append(0)  # Добавляем недостающие ячейки
+    # Сложение чисел
+    for i in range(lenght_mas):
+        for j in range(lenght_mas - 1):  # Идем по столбцам слево направо
+            # Если числа стоят рядом, то правое умножаем на два, а левое удаляем
+            if mas[i][j] == mas[i][j + 1] and mas[i][j] != 0:
+                mas[i][j] *= 2
+                mas[i].pop(j + 1)
+                mas[i].append(0)  # Добавляем недостающие ячейки
+    return mas
+
+
+# При свайпе вправо
+def move_right(mas):
+    lenght_mas = len(mas[0])
+    for row in mas:
+        while 0 in row:
+            row.remove(0)  # Удаляем все нули на поле
+        while len(row) != lenght_mas:
+            row.insert(0, 0)  # Добавляем недостающие ячейки
+    # Сложение чисел
+    for i in range(lenght_mas):
+        for j in range(lenght_mas - 1, 0, -1):  # Идем по столбцам справо налево
+            # Если числа стоят рядом, то правое умножаем на два, а левое удаляем
+            if mas[i][j] == mas[i][j - 1] and mas[i][j] != 0:
+                mas[i][j] *= 2
+                mas[i].pop(j - 1)
+                mas[i].insert(0, 0)  # Добавляем недостающие ячейки
+    return mas
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Logics import *
-import pygame as pg
 import sys
+import numpy
+import pygame as pg
 
 ## Цикл игры:
 # Ждать от пользователя команды
@@ -19,19 +20,23 @@ BLACK = (0, 0, 0)
     0: (130, 130, 130),
     2: (255, 255, 255),
     4: (255, 255, 128),
-    8: (255, 255, 0)
-    }
+    8: (255, 255, 0),
+    16: (255, 235, 255),
+    32: (255, 235, 128),
+    64: (255, 235, 0)
+}
 
-    
+
 def set_2(mas):
     # input()
-    Empty = get_empty_list(mas) # Получаем порядковые номера всех пустых ячеек 
-    rnd.shuffle(Empty) # Мешаем их
-    rnd_num = Empty.pop() # Достаем последнее из списка пустых ячеек
+    Empty = get_empty_list(mas)  # Получаем порядковые номера всех пустых ячеек
+    rnd.shuffle(Empty)  # Мешаем их
+    rnd_num = Empty.pop()  # Достаем последнее из списка пустых ячеек
     x, y = get_index_from_number(mas, rnd_num)
-    mas[x][y] = 2 # Ставим на это место двойку 
+    mas[x][y] = 2  # Ставим на это место двойку
     pretty_print(mas)
-    
+
+
 # Отрисовка игрового поля
 def draw_field(mas, COLORS):
     pg.draw.rect(screen, WHITE, TITILE_REC)
@@ -39,12 +44,12 @@ def draw_field(mas, COLORS):
     for row in range(BLOCKS):
         for column in range(BLOCKS):
             value = mas[row][column]
-            
+
             # Отрисовка ячеек
             w = column * SIZE_BLOCK + (column + 1) * MARGIN
             h = row * SIZE_BLOCK + (row + 1) * MARGIN + SIZE_BLOCK
             pg.draw.rect(screen, COLORS[value], (w, h, SIZE_BLOCK, SIZE_BLOCK))
-            
+
             # Отрисовка цифр
             text = font.render(f'{value}', True, BLACK)
             if value != 0:
@@ -53,14 +58,12 @@ def draw_field(mas, COLORS):
                 text_x = w + (SIZE_BLOCK - font_w) / 2
                 text_y = h + (SIZE_BLOCK - font_h) / 2
                 screen.blit(text, (text_x, text_y))
-            
-            
-            
+
+
 mas = [[0, 0, 0, 0],
        [0, 0, 0, 0],
        [0, 0, 0, 0],
        [0, 0, 0, 0]]
-
 
 # Параметры для графики
 BLOCKS = 4
@@ -75,7 +78,7 @@ pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("2048")
 
-# Проставляем первые две двойки на поле 
+# Проставляем первые две двойки на поле
 set_2(mas)
 set_2(mas)
 
@@ -83,26 +86,24 @@ set_2(mas)
 draw_field(mas, СOLORS)
 pg.display.update()
 
-## Цикл игры
+## Цикл игры ##
 # =============================================================================
 while is_zero_in_mas(mas):
     # Обработчик событий
     for event in pg.event.get():
-        if event.type == pg.QUIT: # Если пользователь нажал на выход
+        if event.type == pg.QUIT:  # Если пользователь нажал на выход
             pg.quit()
-            sys.exit(0) # Закрытие окошка
+            sys.exit(0)  # Закрытие окошка
         elif event.type == pg.KEYDOWN:
-            pretty_print(mas)
+            if event.key == pg.K_LEFT:  # Если нажата кнопка - влево
+                mas = move_left(mas)
+            elif event.key == pg.K_RIGHT:  # Если нажата кнопка - влево
+                mas = move_right(mas)
+            # pretty_print(mas)
             set_2(mas)
-            draw_field(mas, СOLORS) # Перерисовываем поле
+            draw_field(mas, СOLORS)  # Перерисовываем поле
             pg.display.update()
-
-for event in pg.event.get():
-        if event.type == pg.QUIT: # Если пользователь нажал на выход
-            pg.quit()
-            sys.exit(0) # Закрытие окошка
 # =============================================================================
-
 
 
 
